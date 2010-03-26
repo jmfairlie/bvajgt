3e6669f6fceee5e5f85b9b3cc94fa9e09e8dde77
Jean Fairlie <jmfairlie@gmail.com>
2010-03-27 01:26:24 +0200

    removed some unnecessary files cvs files old version of jsWindowContainer old php test files

--- phpconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new COM("ADODB.Connection") or die("Cannot start ADO"); 
-
-// Microsoft Access connection string.
-$conn->Open("DRIVER={Microsoft Access Driver (*.mdb)}; DBQ=C:\\data\\CONTROL03.mdb");
- 
-?>
